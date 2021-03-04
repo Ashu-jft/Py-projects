2a7bc3f442fe9956144e3e2a6a6405f3fe568ac6
Ashu Babbar <ashu.babbar@jellyfishtechnologies.com>
2021-03-04 12:29:42 +0530

    learning theory

---
+++ HelloWorld.py
@@ -0,0 +1,49 @@
+# x=1
+# y=10.0
+# z=1111j
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+
+# print("hello")
+# print('hello')
+# print("hello"=='hello')
+# a="""hi this is ashu
+# ok this is  working"""
+# print(a)
+
+# #####################################
+
+# a = "hello my name is ashu ,how are you"
+#    01234567890123456789012345678901234
+
+# for i in a:
+#     print(i,end='')
+# print()
+
+# hello my n
+# print(len(a))
+
+# myLife="""yo bye"""
+# print("hi" not in myLife)
+
+# boolean
+# can be either true or false
+# a=100
+# b=100
+# print(a==b)
+
+
+# operators
+
+a=10
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+
